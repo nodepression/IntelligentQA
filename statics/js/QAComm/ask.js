985c9ee18f7ff2b605f2448a1065cd4f0fc3fd52
nodepression <qianzgcn@163.com>
2018-07-20 10:24:53 +0800

    1

--- statics/js/QAComm/ask.js
+++ statics/js/QAComm/ask.js
@@ -404,15 +404,6 @@ function clear() {
     $('#tags').val("");
     $('#selected').empty();
 }
-
-
-//提取url参数
-function getUrlParam(name) {
-    var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)"); //构造一个含有目标参数的正则表达式对象
-    var r = window.location.search.substr(1).match(reg);  //匹配目标参数
-    if (r != null) return unescape(r[2]); return null; //返回参数值
-}
-
 //点击问题跳转
 $('#questions').click(function (e) {
     var tagItem;
