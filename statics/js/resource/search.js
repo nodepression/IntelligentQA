5dd15b87eefbc7d193aaed482d46b3f6adbf27aa
nodepression <qianzgcn@163.com>
2018-07-23 17:26:34 +0800

    回答问题bug 修复  回答页翻页

--- statics/js/resource/search.js
+++ statics/js/resource/search.js
@@ -83,6 +83,7 @@
                     if (len <= num) {
                         $('.next').css("visibility", "hidden");
                     }
+                    
                     render();
                 }
             }
